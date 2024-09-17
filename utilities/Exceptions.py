f4daa76c0f2855a97975381feb661cb403b50509
flo.mks <florian.moecks@gmx.de>
2024-09-17 23:34:34 +0200

    + Exception module

---
+++ utilities/Exceptions.py
@@ -0,0 +1,13 @@
+"""
+Module for the Exceptions
+"""
+
+class SpotifyError(Exception):
+    """
+    Base for all spotify related exceptions
+    """
+
+class YouTubeError(Exception):
+    """
+    Base for all YouTube related exceptions
+    """
