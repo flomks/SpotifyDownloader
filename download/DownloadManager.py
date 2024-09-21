76f7ed3b07a271ea890d150f8f5385dcfeba4cfc
flo.mks <florian.moecks@gmx.de>
2024-09-21 10:43:57 +0200

    add DownloadManager module

---
+++ download/DownloadManager.py
@@ -0,0 +1,12 @@
+
+
+class DownloadManager:
+    def __init__(
+            self,
+            client_id,
+            client_secret
+    ) -> None:
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+
