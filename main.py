809d757d388f0e278015c492f76980a30dee9cd6
flo.mks <florian.moecks@gmx.de>
2024-09-21 10:46:05 +0200

    add test stuff

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
-"""m main from ytdownloader.py"""
+"""main from ytdownloader.py"""
 from mutagen.id3 import ID3, ID3NoHeaderError, TIT2, TPE1, TALB, TDRC
 from pytube import YouTube
 from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
-from moviepy.editor import VideoFileClip
+from pydub import AudioSegment
 import os
 import re
 import json
 
 
-OUTPUT_PATH = r'C:\Users\brozm\OneDrive\Desktop\Musik'
+OUTPUT_PATH = r'C:\Users\flori\Desktop\Musik'
 
 
 def set_mp3_metadata(mp3_file, title, artist):
     """Set the ID3-Tags from a mp3-file"""
 
-    try:
 
-        #audio = MP3(mp3_file, ID3=EasyID3)
-        #audio['title'] = title
-        #audio['artist'] = artist
-        audio = ID3(mp3_file)
-        audio['TIT2'] = TIT2(encoding=3, text=title)  # Titel
-        audio['TPE1'] = TPE1(encoding=3, text=artist)  # Künstler
+    audio = MP3(mp3_file, ID3=EasyID3)
+    #audio['title'] = title
+    #audio['artist'] = artist
+    #audio = ID3(mp3_file)
+    audio.tags["TITLE"] = title
+    audio.tags["ARTIST"] = artist
+    audio['TIT2'] = TIT2(encoding=3, text=title)  # Titel
+    audio['TPE1'] = TPE1(encoding=3, text=artist)  # Künstler
 
-        audio.save()
-        #audio.save()
-        return title, artist
+    audio.save()
+    return title, artist
 
-    except Exception as e:
-        print(e)
 
 
 def sanitize_filename(filename):
@@ -45,18 +43,17 @@ def youtube_download_mp3(yt_url):
 
         print("Downloading:", yt.title)
         audio_stream = yt.streams.filter(only_audio=True).first()
+        filename = sanitize_filename(f"{yt.title} - {yt.author}") + ".mp3"
+        audio_stream.download(output_path=OUTPUT_PATH, filename=filename)
 
-        filename = sanitize_filename(f"{yt.title + " - " + yt.author}") + ".mp3"
-        audio_file = audio_stream.download(output_path=OUTPUT_PATH, filename=filename)
+        audio = AudioSegment.from_file(f"{OUTPUT_PATH}\\{filename}").export(f"{OUTPUT_PATH}/{filename}_converted.mp3", format="mp3", bitrate="16k")
 
         # Set Metadata
         artist = "Test"
-        set_mp3_metadata(audio_file, yt.title, artist)
-        print(f"Set MetaData... Done: {yt.title} by {artist}")
 
-        #data = set_mp3_metadata(audio_file, f"{yt.title}.mp3", extract_artist(yt.title))
-        #print("Set MetaData...")
-        #print("Done: " + data[0] + " " + data[1])
+        data = set_mp3_metadata(audio, yt.title, artist)
+        print("Set MetaData...")
+        print("Done: " + data[0] + " " + data[1])
 
     except Exception as e:
         print(e)
@@ -68,7 +65,8 @@ def start():
     with open('music.json', 'r') as file:
         data = json.load(file)
 
-    [youtube_download_mp3(track) for track in data["links"]]
+    #[youtube_download_mp3(track) for track in data["links"]]
+    youtube_download_mp3(r"https://music.youtube.com/watch?v=9TZvAi8pKYw&list=RDAMVM9TZvAi8pKYw")
 
 
 if __name__ == "__main__":
