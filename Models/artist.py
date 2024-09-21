f93e077869ee4fac8f37aab08b00146a51199709
flo.mks <florian.moecks@gmx.de>
2024-09-21 10:45:26 +0200

    add stuff

--- Models/artist.py
+++ Models/artist.py
@@ -6,30 +6,39 @@ from typing import List
 
 from setuptools.extern import names
 
-from album import Album
+
 from spotify.spotify import SpotifyClient
 from utilities.exceptions import UrlError
 
 
 @dataclass
 class Artist:
+
+
     name: str
     id: str
     genres: List[str]
     follower: int
     popularity: int
-    albums: List[Album]
+    albums: List['Album']
+    songs: List['Song']
     spotify_url: str
     uri: str
     href: str
 
     @classmethod
     def from_url(cls, url) -> 'Artist':
+        from album import Album
+        from song import Song
+
         client = SpotifyClient()
         raw_data_artist = client.auth.artist(url)
-        print(raw_data_artist)
 
         if raw_data_artist is None:
+            raise UrlError(r"Invalid artist URL")
+
+        album_raw_data = client.auth.artist_albums(url, limit=None, album_type="album")
+        if album_raw_data is None:
             raise UrlError(r"Invalid artist URL")
 
         return cls(
@@ -39,7 +48,8 @@ class Artist:
             genres=raw_data_artist['genres'],
             follower=raw_data_artist['followers']['total'],
             popularity=raw_data_artist['popularity'],
-            albums=[],
+            albums=[Album.from_url(album["id"]) for album in album_raw_data['items']],
+            songs=[],
             uri=raw_data_artist['uri'],
             href=raw_data_artist['href']
         )
