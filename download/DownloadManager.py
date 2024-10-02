581f663a59436fdab87761864a687fa0cd3d4c12
flo.mks <florian.moecks@gmx.de>
2024-10-02 23:31:36 +0200

    add test case for testing

--- download/DownloadManager.py
+++ download/DownloadManager.py
@@ -1,15 +1,37 @@
-from Models.playlist import Playlist
+
+
+from models.playlist import Playlist
+
+
+from provider.ytmusic import YouTubeMusic
+from utilities.config_handler import Config
+from models.playlist import Playlist
 
 
 class DownloadManager:
     def __init__(
-            self,
-            client_id,
-            client_secret
+            self
     ) -> None:
         self.playlist = None
-        self.client_id = client_id
-        self.client_secret = client_secret
+
     
     def get_playlist(self, url: str):
         self.playlist = Playlist.from_url(url)
+
+
+def test():
+    config = Config()
+    dm = DownloadManager()
+    playlist = Playlist.from_url(config.get_spotify_target_playlist())
+    yt_music = YouTubeMusic()
+
+
+    result = [yt_music.search(track) for track in playlist.songs]
+
+    for r in result:
+        print(r[0])
+
+    print(result)
+
+if __name__ == '__main__':
+    test()
