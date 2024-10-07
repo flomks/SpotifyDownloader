441fa56c427f8178b48292661b0ae2b3a96d1c59
flo <florian.moecks@gmx.de>
2024-10-07 19:40:57 +0200

    + ResultTypes + Categories + add values: id, title

--- models/result.py
+++ models/result.py
@@ -4,32 +4,48 @@ Result module
 from dataclasses import dataclass
 
 from enum import Enum
+from typing import Optional
 
 
 @dataclass
 class ResultBase:
 
     source: str
+    id: str
     url: str
-    duration: int # duration in seconds
+    title: str
+    duration: str # duration in seconds
 
 
 @dataclass
 class YtMusicResult(ResultBase):
-
     class ResultType(Enum):
         video = "video"
         song = "song"
+        album = "album"
+        playlist = "playlist"
+        artist = "artist"
+        profile = "profile"
+        station = "station"
+
 
     class VideoType(Enum):
-        OMV = "MUSIC_VIDEO_TYPE_OMV"
-        UGC = "MUSIC_VIDEO_TYPE_UGC"
-        ATV = "MUSIC_VIDEO_TYPE_ATV"
+        MUSIC_VIDEO_TYPE_OMV = "MUSIC_VIDEO_TYPE_OMV"
+        MUSIC_VIDEO_TYPE_UGC = "MUSIC_VIDEO_TYPE_UGC"
+        MUSIC_VIDEO_TYPE_ATV = "MUSIC_VIDEO_TYPE_ATV"
         OFFICIAL_SOURCE_MUSIC = "OFFICIAL_SOURCE_MUSIC"
+        MUSIC_VIDEO_TYPE_PODCAST_EPISODE = "MUSIC_VIDEO_TYPE_PODCAST_EPISODE"
+
+    class Category(Enum):
+        top_result = "Top result"
+        songs = "Songs"
+        videos = "Videos"
+
+
 
     category: str
+    resultType: ResultType
     streams: str
-    duration: int
     video_type: VideoType
     artists: list[dict[str, str]]
 
